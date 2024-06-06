3d6c34bf7321c13db92028bad31224b3c2dbe32f
Mike Schinkel <mike@newclarity.net>
2024-06-06 05:29:46 -0400

    Remove `isZeroValue` function

--- daemon/shared/validate.go
+++ daemon/shared/validate.go
@@ -68,31 +68,3 @@ func validateField(field reflect.StructField, fieldValue reflect.Value, tag stri
 	}
 	return me.Err()
 }
-
-// Helper function to check if a value is zero value
-func isZeroValue(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Complex64, reflect.Complex128:
-		return v.Complex() == 0
-	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
-		return v.Len() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
-	case reflect.Struct:
-		zero := reflect.New(v.Type())
-		elem := zero.Elem()
-		zeroStruct := elem.Interface()
-		v.IsZero()
-		return v.Interface() == zeroStruct
-	default:
-		return false
-	}
-}
