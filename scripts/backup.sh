10ad104551710de95488a8ebaa1ca8fe44ec07ae
Mike Schinkel <mike@newclarity.net>
2025-01-09 14:32:32 -0500

    Update DB backup script
    - Avoid generating output when mount is not available

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+MOUNT_POINT="/Volumes/Backups"
+ERROR_LOG="${HOME}/savetabs-backup-error-$(date +%Y-%m-%d-%H).log"
 DIR="/Volumes/Backups/Projects/savetabs"
 PREFIX="savetabs"
+
+function ensure_backups_volume_exists {
+  mount | grep -q "^${MOUNT_POINT} " || {
+     printf "Mount %s does not exist at %s\n" \
+      "${MOUNT_POINT}" \
+      "$(date)" >> "$ERROR_LOG"
+     exit
+  }
+}
 function hash {
   local file="$1"
   /usr/local/bin/sha384sum "${file}"  | awk '{print$1}'
@@ -17,7 +28,11 @@ function main {
   srcFile="${HOME}/.config/savetabs/savetabs.db"
   dstFile="${DIR}/${PREFIX}-$(date "+%Y-%m-%d_%H_%M").db"
   logFile="${DIR}log.txt"
+
+  ensure_backups_volume_exists
+
   latestFile="$(latestFile)"
+
 
   srcHash="$(hash "${srcFile}")"
   latestHash="$(hash "${latestFile}")"
